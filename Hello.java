0129bab48be64c3c2a27290be8ded5f9f3aa9c8d
Rajesh <rajesh630@gmail.com>
2026-08-13 16:09:14 +0530

    Revert "Hello.java updated"
    This reverts commit c5ac60804f0b5624123f58e381d1a031fe0981b1.

--- Hello.java
+++ Hello.java
@@ -5,7 +5,6 @@ class Hello
 		System.out.println("Hello World!");
 		System.out.println("Hello Github");
 		System.out.println("Hello World!11");
-        System.out.println("hi ra ");
-		System.out.println("Hello Github");	
-	}
+        System.out.println("hi ra ")
+		System.out.println("Hello Github ");	}
 }
